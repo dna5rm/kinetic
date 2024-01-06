36a4b85810afd3c64f5c6456a6d959cbde20aec0
David Eaves <null@crc1.net>
2024-01-06 16:11:45 -0500

    2024-01-06 16:11:45

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from database import engine
 import models
 from routers import agents, hosts, monitors, agent_jobs, console
 from starlette.staticfiles import StaticFiles
+from datetime import datetime
 
+# Create FastAPI instance
 app = FastAPI(
     title="Kinetic",
     description="Kinetic is a network monitoring tool",
@@ -22,6 +24,13 @@ models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Set server start time
+server_start_time = datetime.now()
+
+# Set server start time in app
+app.server_start_time = server_start_time
+
+
 # Basic CRUD operations
 app.include_router(agents.router)
 app.include_router(hosts.router)
@@ -30,18 +39,18 @@ app.include_router(monitors.router)
 # Agent job operations
 app.include_router(agent_jobs.router)
 
+# App UI operations pass server_start_time to console.router
+app.include_router(console.router)
+
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     """ Favicon """
     return None
 
-# App UI operations
-app.include_router(console.router)
-
-#@app.get("/", include_in_schema=False)
-#async def root():
-#    """ Root """
-#    raise HTTPException(status_code=200, detail="Kinetic is running")
+@app.get("/", include_in_schema=False)
+async def root():
+    """ Root level redirection to /console """
+    raise HTTPException(status_code=307, detail="Redirecting to /console", headers={"Location": "/console"})
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8080)
--- routers/console.py
+++ routers/console.py
@@ -7,18 +7,20 @@ from starlette.responses import RedirectResponse
 
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import Depends, APIRouter, Request
+from fastapi import Depends, APIRouter, Request, HTTPException
+from fastapi.responses import HTMLResponse
 from models import Agents, Hosts, Monitors
 from database import SessionLocal
-
+from datetime import datetime
+from humanize import naturaldelta
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
-    #prefix="/",
+    prefix="/console",
     tags=["console"],
     responses={404: {"description": "Not found"}},
-    include_in_schema=True # Will be set to false later
+    include_in_schema=False
 )
 
 def get_db():
@@ -33,18 +35,51 @@ DBDependency = Annotated[Session, Depends(get_db)]
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
-async def home(request: Request, db: DBDependency):
+async def console_home(request: Request, db: DBDependency):
 
-    # Get a list of all the active agents from db
-    agent_list = db.query(Agents)
+    # get a list of all agents where is_active is True
+    agents = db.query(Agents).filter(Agents.is_active == True).all()
 
     # Create context dictionary with app title
     context = {
         "request": request,
         "title": request.app.title,
         "description": request.app.description,
-        "agent_list": agent_list
+        "agents": agents,
+        "stats": {
+            "active_agents": len(agents),
+            "active_hosts": db.query(Hosts).filter(Hosts.is_active == True).count(),
+            "active_monitors": db.query(Monitors).filter(Monitors.is_active == True).count(),
+            "total_agents": db.query(Agents).count(),
+            "total_hosts": db.query(Hosts).count(),
+            "total_monitors": db.query(Monitors).count(),
+            "server_start_time": request.app.server_start_time,
+            "server_run_time": naturaldelta(datetime.now() - request.app.server_start_time)
+        }
     }
 
     # Include the app title in response.
     return templates.TemplateResponse("home.html", context=context)
+
+@router.get("/agent/{agent_id}", response_class=HTMLResponse)
+# get agent_id from path and pass it to console_agent
+async def console_agent(request: Request, agent_id: int, db: DBDependency):
+    """ Console Agent """
+    # get agent from database by agent_id
+    agent = db.query(Agents).filter(Agents.id == agent_id).first()
+
+    # if agent esists and is active
+    if agent and agent.is_active:
+
+        # Create context dictionary with active monitor data
+        context = {
+            "request": request,
+            "title": request.app.title,
+            "agent": agent,
+            "monitor": db.query(Monitors).filter(Monitors.agent_id == agent_id, Monitors.is_active == True).all()
+        }
+
+        return templates.TemplateResponse("agent.html", context=context)
+        
+    # Default raise HTTPException with 404 status code
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"agent_id not found")
