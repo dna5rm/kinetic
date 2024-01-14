de569409533d231c0c33c5c4336f26ad06aa34f3
David Eaves <null@crc1.net>
2024-01-13 20:51:38 -0500

    2024-01-13 20:51:38

--- volley.py
+++ volley.py
@@ -12,10 +12,10 @@ import requests
 import time
 import logging
 
-# Set the logging formatting
+# Set the logging formatting and level
 logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',level=logging.INFO)
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-logging.disable(logging.DEBUG)
+#logging.disable(logging.DEBUG)
 
 class ReadJobInput(BaseModel):
     id: str = Field(..., pattern="^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$", description="Monitor by id")
